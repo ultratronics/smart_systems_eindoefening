f604d5a830fe1de230b1fb0fda35c08a07b12cc5
Kristof <kristof_heulsen@outlook.com>
2020-01-06 15:38:07 +0100

    add download folder met al

---
+++ Download_Project/automatisch_pickle/project.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Automatische Streamlit app
+
+# In[1]:
+
+
+import pickle
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+def checkIfStringEmpty(var):
+    if var == "":
+        return 0
+    else:
+        return var
+
+model =pickle.load(open("./BreastCancerPickle.pkl","+rb"))
+test = pd.read_csv("./wisc_bc_data(1).csv")
+st.write(test.dtypes)
+aantal_kolommen = len(test.columns)
+output_categorie = "diagnosis"
+index_output = test.columns.get_loc(output_categorie)
+inputlist = []
+list1 = []  # np.empty((0,aantal_kolommen-1))
+outputnaam = ""
+for i in range(0, aantal_kolommen):
+    # print(i)
+    # print(index_output)
+    if i == index_output:
+        list1.append("output")
+    else:
+        list1.append("input")
+print(list1)
+
+st.write("hallo")
+
+
+for j in range(0, aantal_kolommen):
+    print(j)
+    # print(index_output)
+    if list1[j] == "input":
+        if test.dtypes[j] == "float64":
+            # inputlist.insert(j, st.text_input(test.columns[j], key=j))
+            temp = st.text_input(test.columns[j], key=j)
+            inputlist.insert(j, float(checkIfStringEmpty(temp)))
+        if test.dtypes[j] == "int64":
+            temp = st.text_input(test.columns[j], key=j)
+            inputlist.insert(j, int(checkIfStringEmpty(temp)))
+    if list1[j] == "output":
+        outputnaam = test.columns[j]
+        inputlist.insert(j, "output")
+
+
+
+X_test = pd.DataFrame(inputlist)
+X_test = X_test.drop(index_output)
+st.write(inputlist)
+st.write(type(inputlist))
+
+st.write(X_test)
+
+resultaat = model.predict(X_test.T)
+st.write(resultaat)
+
+
+get_ipython().system("jupyter nbconvert --output-dir='./push' --to script autopickl.ipynb")
+get_ipython().system('head -n -7 ./push/autopickl.py > ../automatisch_pickle/project.py')
+get_ipython().system('git -C ../automatisch_pickle commit -am "Nieuw commit" | git -C ../automatisch_pickle push heroku master')
+get_ipython().system('git -C ../automatisch_pickle commit -am "Nieuw commit" | git -C ../automatisch_pickle push heroku master')
---
+++ Download_Project/steamlit_demoprogrammas/UberPickup/README.md
@@ -0,0 +1,12 @@
+# Streamlit Demo: Uber Pickups in New York City
+A [Streamlit](https://streamlit.io) demo [written in pure Python](https://github.com/streamlit/demo-uber-nyc-pickups/blob/master/app.py) to interactively visualize Uber pickups in New York City.
+
+## How to run this demo
+```
+pip install --upgrade streamlit
+streamlit run https://raw.githubusercontent.com/streamlit/demo-uber-nyc-pickups/master/app.py
+```
+
+### Questions? Comments?
+
+Please ask in the [Streamlit community](https://discuss.streamlit.io).
